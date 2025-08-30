46ca1798242bd10ffb1fce1d7eb856a1d2994189
Sannidhi Ranga Sudeeksha <srsudeeksha@gmail.com>
2025-08-30 14:56:12 +0530

    DataTypes

---
+++ 2 - datatypes.py
@@ -0,0 +1,74 @@
+# String
+a = "Hello World"
+print("a =", a)
+print("Type of a:", type(a))
+
+# Integer
+b = 20
+print("\nb =", b)
+print("Type of b:", type(b))
+
+# Float
+c = 20.5
+print("\nc =", c)
+print("Type of c:", type(c))
+
+# Complex
+d = 1j
+print("\nd =", d)
+print("Type of d:", type(d))
+
+# List
+e = ["apple", "banana", "cherry"]
+print("\ne =", e)
+print("Type of e:", type(e))
+
+# Tuple
+f = ("apple", "banana", "cherry")
+print("\nf =", f)
+print("Type of f:", type(f))
+
+# Range
+g = range(6)
+print("\ng =", g)
+print("Type of g:", type(g))
+
+# Dictionary
+h = {"name": "John", "age": 36}
+print("\nh =", h)
+print("Type of h:", type(h))
+
+# Set
+i = {"apple", "banana", "cherry"}
+print("\ni =", i)
+print("Type of i:", type(i))
+
+# Frozenset
+j = frozenset({"apple", "banana", "cherry"})
+print("\nj =", j)
+print("Type of j:", type(j))
+
+# Boolean
+k = True
+print("\nk =", k)
+print("Type of k:", type(k))
+
+# Bytes
+l = b"Hello"
+print("\nl =", l)
+print("Type of l:", type(l))
+
+# Bytearray
+m = bytearray(5)
+print("\nm =", m)
+print("Type of m:", type(m))
+
+# Memoryview
+n = memoryview(bytes(5))
+print("\nn =", n)
+print("Type of n:", type(n))
+
+# NoneType
+o = None
+print("\no =", o)
+print("Type of o:", type(o))
