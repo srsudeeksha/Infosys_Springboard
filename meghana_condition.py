b822be6514ede9cbea6359b105c6a5b895e82a3d
Sannidhi Ranga Sudeeksha <srsudeeksha@gmail.com>
2025-08-30 15:22:45 +0530

    Meghana Condition

---
+++ meghana_condition.py
@@ -0,0 +1,7 @@
+# Meghana condition problem
+meghana_died = input("Did Meghana die? (yes/no): ").lower()
+
+if meghana_died == "yes":
+    print("Surya meets Priya")
+else:
+    print("Meghana weds Surya")
